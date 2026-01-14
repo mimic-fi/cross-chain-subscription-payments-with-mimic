0c4a9fcc330bbabd1089770c4d4070cbb211d1e6
patricio henderson <patricio.henderson.v@gmail.com>
2026-01-14 08:37:34 -0300

    chore: init project

---
+++ packages/tasks/tests/task.spec.ts
@@ -0,0 +1,60 @@
+import { Chains, fp, ONES_ADDRESS, OpType, ZERO_ADDRESS } from '@mimicprotocol/sdk'
+import { EvmCallQueryMock, runTask, Transfer } from '@mimicprotocol/test-ts'
+import { expect } from 'chai'
+
+describe('Task', () => {
+  const taskDir = './build'
+
+  describe('when the chain is supported', () => {
+    const chainId = Chains.Optimism
+
+    const context = {
+      user: '0x756f45e3fa69347a9a973a725e3c98bc4db0b5a0',
+      settlers: [{ address: '0xdcf1d9d12a0488dfb70a8696f44d6d3bc303963d', chainId }],
+      timestamp: Date.now(),
+    }
+
+    describe('when source and destination chain are the same', () => {
+      const inputs = {
+        sourceChain: chainId,
+        destinationChain: chainId,
+        amountIn: '1.5', // 1.5 USDC
+        payer: ONES_ADDRESS,
+        recipient: ZERO_ADDRESS,
+        slippage: '0.5', // 0,5 %
+      }
+
+      const calls: EvmCallQueryMock[] = [
+        {
+          request: { to: inputs.token, chainId, fnSelector: '0x313ce567' }, // decimals
+          response: { value: '6', abiType: 'uint8' },
+        },
+      ]
+
+      it('produces the expected intents', async () => {
+        const result = await runTask(taskDir, context, { inputs, calls })
+        expect(result.success).to.be.true
+        expect(result.timestamp).to.be.equal(context.timestamp)
+
+        const intents = result.intents as Transfer[]
+        expect(intents).to.have.lengthOf(1)
+
+        expect(intents[0].op).to.be.equal(OpType.Transfer)
+        expect(intents[0].settler).to.be.equal(context.settlers[0].address)
+        expect(intents[0].user).to.be.equal(context.user)
+        expect(intents[0].chainId).to.be.equal(inputs.chainId)
+        expect(intents[0].maxFees).to.have.lengthOf(1)
+        expect(intents[0].maxFees[0].token).to.be.equal(inputs.token)
+        expect(intents[0].maxFees[0].amount).to.be.equal(fp(inputs.maxFee, 6).toString())
+
+        expect(intents[0].transfers).to.have.lengthOf(1)
+        expect(intents[0].transfers[0].token).to.be.equal(inputs.token)
+        expect(intents[0].transfers[0].amount).to.be.equal(fp(inputs.amount, 6).toString())
+        expect(intents[0].transfers[0].recipient).to.be.equal(inputs.recipient)
+      })
+    })
+
+    describe('when source an destination chain are different', () => {})
+  })
+  describe('when the chain is not supported', () => {})
+})
